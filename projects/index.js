3abcf33148314734ef2ff734b20711df642e6dab
samuellehikoinen <samuel.lehikoinen@gmail.com>
2022-02-06 15:10:22 +0200

    Add jquery

---
+++ projects/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+    $(".git-oura").click(function () {
+        window.open("https://github.com/samuellehikoinen/oura_tracker", '_blank').focus();
+    });
+});
+
+$(document).ready(function () {
+    $(".project-oura").click(function () {
+        // Open modal
+        console.dir("A modal should open now");
+    });
+});
