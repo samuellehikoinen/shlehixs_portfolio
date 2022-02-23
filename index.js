ee9818b356d8b103b07647ad01d0a8441ca7c11a
samuellehikoinen <samuel.lehikoinen@gmail.com>
2022-02-23 20:50:21 +0200

    Add navbar

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+    $(".home").hover(function () {
+        $(".home i").removeClass("fa-door-closed").addClass("fa-door-open");
+    }, function () {
+        $(".home i").removeClass("fa-door-open").addClass("fa-door-closed");
+    });
+});
